387cc8041be49ae2918a92d52e1c66b8bfe3ba42
Noel Abellanosa <abellnoel99@gmail.com>
2024-05-15 00:14:42 -0400

    feat: add basic gallery card styles

--- src/components/GalleryCard.vue
+++ src/components/GalleryCard.vue
@@ -1,16 +1,32 @@
-﻿<script setup lang="ts"></script>
+﻿<script setup lang="ts">
+defineProps(['image']);
+</script>
+
 <template>
 	<div class="gallery-card">
-		<h3>Coming soon...</h3>
+		<img
+			class="image"
+			src="https://picsum.photos/1000/2000"
+			alt="TODO:ADD ALT TEXT"
+		/>
+		<h3>Temp Title</h3>
 	</div>
 </template>
 
 <style scoped>
 .gallery-card {
+	display: flex;
+	flex-direction: column;
 	width: 100%;
 	max-width: 20rem;
 	height: 30rem;
-	border: 1px solid white;
+	background-color: var(--text);
+	box-shadow: 10px 5px 20px black;
+}
+
+.image {
+	margin: 1.5rem 1.5rem 0 1.5rem;
+	height: 80%;
 }
 
 h3 {
